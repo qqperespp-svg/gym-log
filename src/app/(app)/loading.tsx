b93a0acd471718f3123b641c575cdf95b83d8a02
qqperespp-svg <qqperespp@gmail.com>
2026-08-17 16:08:55 +0200

    Add files via upload

---
+++ src/app/(app)/loading.tsx
@@ -0,0 +1,7 @@
+export default function AppLoading() {
+  return (
+    <div className="flex h-64 items-center justify-center">
+      <div className="h-9 w-9 animate-spin rounded-full border-[3px] border-zinc-800 border-t-lime-400" />
+    </div>
+  );
+}

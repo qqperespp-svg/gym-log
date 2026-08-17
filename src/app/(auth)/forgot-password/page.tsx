8c7d114e80857d7294c1afdade87af2784e9a557
qqperespp-svg <qqperespp@gmail.com>
2026-08-17 23:42:05 +0200

    Delete src directory

--- src/app/(auth)/forgot-password/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-import { ForgotPasswordForm } from "@/components/forgot-password-form";
-import { getSessionUser } from "@/lib/auth";
-
-export default async function ForgotPasswordPage() {
-  if (await getSessionUser()) redirect("/dashboard");
-  return <ForgotPasswordForm />;
-}

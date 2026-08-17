8327cca5898956e508e342d71e51e1f1011c40c2
qqperespp-svg <qqperespp@gmail.com>
2026-08-17 23:14:30 +0200

    Delete loading.tsx

--- loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function AppLoading() {
-  return (
-    <div className="flex h-64 items-center justify-center">
-      <div className="h-9 w-9 animate-spin rounded-full border-[3px] border-zinc-800 border-t-lime-400" />
-    </div>
-  );
-}

69b81d99092fcb417cc8de86f6d47dcaf723805f
qqperespp-svg <qqperespp@gmail.com>
2026-08-17 19:17:56 +0200

    Delete page (14).tsx

--- page (14).tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-import { AuthForm } from "@/components/auth-form";
-import { getCurrentUser } from "@/lib/auth";
-
-export default async function RegisterPage() {
-  if (await getCurrentUser()) redirect("/dashboard");
-  return <AuthForm mode="register" />;
-}

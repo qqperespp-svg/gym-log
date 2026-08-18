bc1444274106550f85b7c3f7a5887b3da2ff2507
qqperespp-svg <qqperespp@gmail.com>
2026-08-18 09:09:25 +0200

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getSessionUser } from "@/lib/auth";
-import { redirect } from "next/navigation";
-
-export default async function HomePage() {
-  const user = await getSessionUser();
-  redirect(user ? "/dashboard" : "/login");
-}
